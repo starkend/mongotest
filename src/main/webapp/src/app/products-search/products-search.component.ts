12a3f1d59e5a8f64cc2df36472d01e06e3be86ee
starkend <danstarken@gmail.com>
2019-08-17 09:20:34 -0500

    Cleared out product search page proof of concept code to prepare for populating with API data.

--- src/main/webapp/src/app/products-search/products-search.component.ts
+++ src/main/webapp/src/app/products-search/products-search.component.ts
@@ -7,23 +7,11 @@ import {ProductsService} from "../shared/service/products.service";
   styleUrls: ['./products-search.component.css']
 })
 export class ProductsSearchComponent implements OnInit {
-  products: Array<any>;
-  productColumns: Array<any>;
 
   constructor(private productsService: ProductsService) {
   }
 
   ngOnInit() {
-    this.getProductList();
-    this.productColumns = ['name','brandName'];
-  }
-
-  getProductList() {
-    this.productsService.getProductList().subscribe(
-      data => {
-        this.products = data;
-      }
-    );
   }
 
 }

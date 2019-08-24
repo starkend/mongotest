5ea698b3527f7225b7eeca29190ab7e59677a9de
starkend <danstarken@gmail.com>
2019-08-24 09:55:51 -0500

    Code cleanup

--- src/main/java/com/starkend/mongotest/util/ProductUtils.java
+++ src/main/java/com/starkend/mongotest/util/ProductUtils.java
@@ -3,7 +3,6 @@ package com.starkend.mongotest.util;
 import com.starkend.mongotest.dto.ProductDto;
 import com.starkend.mongotest.model.Product;
 import org.springframework.beans.BeanUtils;
-import org.springframework.context.annotation.Bean;
 
 public class ProductUtils {
 
